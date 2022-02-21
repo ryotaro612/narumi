6422c4de3ea8ba972554cffa5ec2b97a529614e5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-02-21 11:17:55 +0900

    Run M-q(fill-paragraph) on ;;; Commentary block. https://github.com/melpa/melpa/pull/7906#issuecomment-1046293884

--- narumi-mode.el
+++ narumi-mode.el
@@ -23,11 +23,11 @@
 
 ;;; Commentary:
 
-;; narumi-mode is a major mode that shows a dashboard.
-;; It shows random sampled image in a directry, bookmarks, and recent files.
-;; You can change the wallpaper to the image on the dashboard.
-;; Since it is a dashboard,
-;; you can go to the entries of bookmarks and recent files by clicking them.
+;; narumi-mode is a major mode that shows a dashboard.  It shows
+;; random sampled image in a directry, bookmarks, and recent files.
+;; You can change the wallpaper to the image on the dashboard.  Since
+;; it is a dashboard, you can go to the entries of bookmarks and
+;; recent files by clicking them.
 (require 'bookmark)
 (require 'recentf)
 (require 'seq)
